bc3e8da2b90535047f11b74199adcf24bc018ea4
akshayUniverse <akshaykarada72.o@gmail.com>
2024-11-09 18:00:04 +0530

    Initial commit with backend setup and .env ignored

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+
+require('dotenv').config({ path: './backend/.env' });
+const mongoose = require('mongoose');
+
+
+const express = require('express');  // Import express
+
+const app = express(); // initialize express app
+
+const PORT = process.env.PORT || 5000; // define the port
+
+const DB_CONNECTION = process.env.DB_CONNECTION;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+app.get('/',(req,res)=>{               // basic route for testing
+
+    res.send('Server is running successfully!');
+});
+
+app.listen(PORT,()=>{                  //start the server
+
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+mongoose.connect(process.env.DB_CONNECTION)
+.then(()=>console.log("Database connected successfully"))
+.catch((error)=> console.error("Database connection error:",error));
+
+
+
+
